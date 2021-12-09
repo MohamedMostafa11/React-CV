65bb27492d7d7b6917f3a1f6981853ada385c17d
MohamedMostafa11 <73854777+MohamedMostafa11@users.noreply.github.com>
2021-12-09 20:21:35 +0200

    Uploaded files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Content from './components/Content';
+import Last from './components/Last';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Content />
+      <Last />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+export default function NavBar() {
+    const linkes = ["Home", "About", "Portfolio", "Store"];
+  
+    return (
+      <header>
+        <div className="container">
+          <div className="logo"> KG </div>
+          <ul>
+            {linkes.map((link) => (
+              <li key={link}>
+                <a href="#">{link}</a>
+              </li>
+            ))}
+          </ul>
+          <span className="lang">AR</span>
+        </div>
+      </header>
+    );
+  }
